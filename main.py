f1aa72929f6d4886bb1fe61d44ba35aaa8655cc0
Nikhil Popli <97437109+nikp1172@users.noreply.github.com>
2022-09-28 17:46:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import random
 import sys
 from time import sleep
 
-for x in range(15): 
-    sleep(1)
-    print("hello world")
+for i in range(10):
+    for x in range(15): 
+        sleep(1)
+        print("hello world")
+    sleep(20)
 code = random.choice([0,1])
 sys.exit(code)
